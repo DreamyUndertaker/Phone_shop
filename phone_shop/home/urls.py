3a908ed6b5a93a803dead4d800de69f49aea1b0b
Dreamy Undertaker <vadiimreg73@gmail.com>
2023-12-22 02:30:16 +0400

    Frontend ready

--- phone_shop/home/urls.py
+++ phone_shop/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('catalog', views.catalog, name='catalog'),
+    path('about', views.about, name='about'),
+    path('shipping', views.shipping, name='shipping'),
+    path('blog', views.blog, name='blog'),
+    path('contacts', views.contacts, name='contacts')
 ]
--- phone_shop/home/views.py
+++ phone_shop/home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+def catalog(request):
+    return render(request, 'home/catalog.html')
+def about(request):
+    return render(request, 'home/about.html')
+def shipping(request):
+    return render(request, 'home/shipping.html')
+def blog(request):
+    return render(request, 'home/blog.html')
+def contacts(request):
+    return render(request, 'home/contacts.html')
